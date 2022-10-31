9271a51ab0b0b43f052e160e6ec3cdd702856f53
Nermine Maddouri <62077915+NerminSMaddouri@users.noreply.github.com>
2022-10-31 20:44:47 +0100

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten 
+from keras.optimizers import Adam   
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization,AveragePooling2D
 from keras.losses import categorical_crossentropy
 from keras.regularizers import l2
